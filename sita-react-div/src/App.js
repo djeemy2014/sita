ab0899d0614ae73034478eb1d26799d3ce14fbd9
Dmitiy Demidyuk work <djeemy2014@gmail.com>
2023-09-05 10:30:10 +0300

    start...

--- sita-react-div/src/App.js
+++ sita-react-div/src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Cesium version 3.0 =P</h1>
+        <h2>Sita</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
