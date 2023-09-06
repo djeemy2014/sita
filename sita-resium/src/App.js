b85cdbbb4e299dd959a0fae36814b5c5df20d718
Demidyuk Dmitiy <djeemy2014@gmail.com>
2023-09-06 22:57:08 +0300

    +/-

--- sita-resium/src/App.js
+++ sita-resium/src/App.js
@@ -9,14 +9,17 @@ import {
   Camera, 
   Scene, 
   Primitive, 
-  Cesium3DTileset 
+  Cesium3DTileset,
+  CameraFlyTo 
 } from "resium";
 import { Cartesian3 } from "cesium";
 import {
   Ion, IonResource,
   createWorldTerrainAsync, 
   UrlTemplateImageryProvider, 
-  createOsmBuildingsAsync } from "cesium"
+  createOsmBuildingsAsync,
+  Matrix4 
+} from "cesium"
 
 Ion.defaultAccessToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3NzJjNjYzYi1jMmMzLTQ4YmMtYjQ3OC0zOTFhZjE4MWFlNmMiLCJpZCI6NjQyMjUsImlhdCI6MTY5Mzk5ODY4NX0.ptWchwMm8LuwnypYqoS1T4hSZ2JxKFxAcioki5FZczU"
 const terrainProvider = await createWorldTerrainAsync();
@@ -25,16 +28,25 @@ const osm = UrlTemplateImageryProvider({
 })
 
 const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100);
-const position_cam = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 10000);
+const position_cam = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 10000000);
+const position_cam_2 = Cartesian3.fromDegrees(90,90,90);
+const position_cam_3 = Cartesian3.fromDegrees(-90,-90,-90);
+/* const orientation_cam = {
+  heading: Cesium.Math.toRadians(20.0),
+  pitch: Cesium.Math.toRadians(20.0),
+  roll:0
+}; */
 
 function App() {
-  
+  let Camera_0=Camera
+  //Camera_0.defaultProps.position=position_cam
+  //console.log(Camera_0.defaultProps.direction)
   return (
     <Viewer  full  timeline={false} terrainProvider={terrainProvider} baseLayer={osm} >
-      <Cesium3DTileset url={IonResource.fromAssetId(96188)} />
+      {/* <Cesium3DTileset url={IonResource.fromAssetId(96188)} /> */}
       <Scene>
-      <Camera position={position_cam}>
-      </Camera>
+      <Camera_0 position={position_cam}>
+      </Camera_0>
       <Entity position={position} name="Tokyo">
         <PointGraphics pixelSize={10} />
         <EntityDescription>
